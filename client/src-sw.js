b9cf61b59c1d259884061ee7ec83decd82cdafa6
codejoes <114871953+codejoes@users.noreply.github.com>
2023-04-03 07:36:59 -0400

    adding offline fallback

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,14 +1,14 @@
-const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst } = require('workbox-strategies');
-const { registerRoute } = require('workbox-routing');
-const { CacheableResponsePlugin } = require('workbox-cacheable-response');
-const { ExpirationPlugin } = require('workbox-expiration');
-const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
+const { warmStrategyCache } = require("workbox-recipes");
+const { CacheFirst } = require("workbox-strategies");
+const { registerRoute, setCatchHandler } = require("workbox-routing");
+const { CacheableResponsePlugin } = require("workbox-cacheable-response");
+const { ExpirationPlugin } = require("workbox-expiration");
+const { precacheAndRoute } = require("workbox-precaching/precacheAndRoute");
 
 precacheAndRoute(self.__WB_MANIFEST);
 
 const pageCache = new CacheFirst({
-  cacheName: 'page-cache',
+  cacheName: "page-cache",
   plugins: [
     new CacheableResponsePlugin({
       statuses: [0, 200],
@@ -20,11 +20,27 @@ const pageCache = new CacheFirst({
 });
 
 warmStrategyCache({
-  urls: ['/index.html', '/'],
+  urls: ["/index.html", "/"],
   strategy: pageCache,
 });
 
-registerRoute(({ request }) => request.mode === 'navigate', pageCache);
+registerRoute(({ request }) => request.mode === "navigate", pageCache);
 
 // TODO: Implement asset caching
+self.addEventListener("install", (event) => {
+  const files = ["/offline.html"];
+  event.waitUntil(
+    self.caches.open("offline-fallbacks").then((cache) => cache.addAll(files))
+  );
+});
+
+setCatchHandler(async (options) => {
+  const destination = options.request.destination;
+  const cache = await self.caches.open("offline-fallbacks");
+  if (destination === "document") {
+    return (await cache.match("/offline.html")) || Response.error();
+  }
+  return Response.error();
+});
+
 registerRoute();
